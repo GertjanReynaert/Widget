c3201bda85b6616b4e68df32691c789517d8ab46
Gertjan Reynaert <gertjan@typework.com>
2014-10-21 18:08:45 +0200

    Edit item text

--- app/react/x-init.js
+++ app/react/x-init.js
@@ -1,13 +1,12 @@
 /** @jsx React.DOM */
-// var firebaseAdapter = new FirebaseAdapter('https://presentationwizard.firebaseio.com/web/saving-data/presentationwizard/');
+var firebaseAdapter = new FirebaseAdapter('https://presentationwizard.firebaseio.com/web/saving-data/presentationwizard/');
 var localStorageAdapter = {
   rules: function() {
     return navigator.onLine;
   },
   adapter: new LSAdapter('widget')
 };
-// var adapters = [localStorageAdapter, firebaseAdapter];
-var adapters = [localStorageAdapter];
+var adapters = [localStorageAdapter, firebaseAdapter];
 var serializers = [ListSerializer];
 
 var data = new Data(adapters, serializers);
